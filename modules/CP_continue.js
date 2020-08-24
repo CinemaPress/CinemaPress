ea40004f29ad65bc4b9e410a3ff7dcc4effeff2d
zeldaroot <zeldaroot@protonmail.com>
2020-08-24 04:39:08 +0300

    Fixed ru continue

--- modules/CP_continue.js
+++ modules/CP_continue.js
@@ -15,13 +15,13 @@ var modules = require('../config/production/modules');
  * @return {String}
  */
 
-function codeContinue() {
+function codeContinue(options) {
   var code = '';
 
   if (modules.continue.status) {
     code =
       'var continue_domain="' +
-      config.domain +
+      options.domain +
       '";' +
       'function continueViewing(){var e=getCookieCinemaPress("CP_continue");e&&(window.location.href=e)}function watchLater(){document.querySelector("#watchLater").textContent="✔ OK",setCookieCinemaPress("CP_continue",window.location.href.split("?")[0].split("#")[0].split(/\\/s[0-9]*?e[0-9]*?_[0-9]*?$/)[0],{expires:31104e3,path:"/",domain:continue_domain})}!function(){var t=document.querySelectorAll(".continueViewingBlock");if(t&&t.length&&getCookieCinemaPress("CP_continue"))for(var n=0;n<t.length;n++)t[n].style.display="block";var i=document.querySelector("#continueViewing");i&&i.addEventListener("click",continueViewing);var o=document.querySelectorAll(".watchLaterBlock");if(o&&o.length)for(var c=0;c<o.length;c++)o[c].style.display="block";var r=document.querySelector("#watchLater");r&&r.innerHTML;r&&r.addEventListener("click",function t(){watchLater(),r.removeEventListener("click",t)})}();';
 
