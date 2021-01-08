66042314080cf6487cb1831d5517e39c70c478ea
zeldaroot <zeldaroot@protonmail.com>
2021-01-08 02:33:21 +0200

    Changed limit req for bots

--- config/default/nginx/nginx.sh
+++ config/default/nginx/nginx.sh
@@ -4,6 +4,7 @@ for NGINX_DIR in /home/*/config/production/nginx/conf.d/default.conf; do
   NGINX_INIT=$(md5sum "${NGINX_DIR}" | awk '{ print $1 }')
   NGINX_NOW=${NGINX_INIT}
   while true; do
+    [ -f "${NGINX_DIR}" ] || continue
     NGINX_NOW=$(md5sum "${NGINX_DIR}" | awk '{ print $1 }')
     if [ "${NGINX_INIT}" != "${NGINX_NOW}" ]; then
       echo "Reloading... [${NGINX_DIR}]"
