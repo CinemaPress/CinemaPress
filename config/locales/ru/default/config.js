71596e31db45fcf211b213c08b1499beda76d4be
zeldaroot <zeldaroot@protonmail.com>
2021-01-16 04:36:24 +0200

    Changed mail user-agent

--- config/locales/ru/default/config.js
+++ config/locales/ru/default/config.js
@@ -37,7 +37,7 @@ module.exports = {
     "yahoo ~ crawl.yahoo.net ~ ~ Fake Yahoo Bot!",
     "baidu ~ baidu.com,baidu.jp ~ ~ Fake Baidu Bot!",
     "duckduckgo ~ ~ 23.21.227.69,40.88.21.235,50.16.241.113,50.16.241.114,50.16.241.117,50.16.247.234,52.204.97.54,52.5.190.19,54.197.234.188,54.208.100.253,54.208.102.37,107.21.1.8 ~ Fake DuckDuckGo Bot!",
-    "mail ~ mail.ru ~ ~ Fake Mail Bot!"
+    "mail.ru ~ mail.ru ~ ~ Fake Mail Bot!"
   ],
   "defense": {
     "domain": 2,
