84105189f7d685635030a1f1fbddbba010ea4ff7
zeldaroot <zeldaroot@protonmail.com>
2021-01-18 06:39:36 +0200

    Changed ur file

--- cinemapress.sh
+++ cinemapress.sh
@@ -4025,10 +4025,10 @@ while [ "${WHILE}" -lt "2" ]; do
                     rm -f "/home/${DD_MIN}/.uptimerobot"
                 done
             fi
-            [ ! -f "/home/${2}/.uptimerobot" ] || exit 0
-            touch "/home/${2}/.uptimerobot"
             if [ -n "${2}" ] && [ "${2}" != "push" ]; then
                 [ -f "/home/${2}/app.js" ] || [ -f "/home/${2}/index.php" ] || exit 0
+                [ ! -f "/home/${2}/.uptimerobot" ] || exit 0
+                touch "/home/${2}/.uptimerobot"
                 sleep $(( ( "${RANDOM}" % 10 ) + ( "${RANDOM}" % 10 ) + ( "${RANDOM}" % 10 ) ))
                 DD=${2}
                 DD_=$(echo "${2}" | sed -r "s/[^A-Za-z0-9]/_/g")
