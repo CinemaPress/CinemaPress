9aea31375e9ec5ade439e69e6d1b17f4d4e508a1
zeldaroot <zeldaroot@protonmail.com>
2021-03-26 03:36:20 +0200

    Added new DDG IP

--- config/locales/en/default/config.js
+++ config/locales/en/default/config.js
@@ -40,7 +40,7 @@ module.exports = {
     "bing ~ search.msn.com ~ ~ Fake Bing Bot!",
     "yahoo ~ crawl.yahoo.net ~ ~ Fake Yahoo Bot!",
     "baidu ~ baidu.com,baidu.jp ~ ~ Fake Baidu Bot!",
-    "duckduckgo ~ ~ 23.21.227.69,40.88.21.235,50.16.241.113,50.16.241.114,50.16.241.117,50.16.247.234,52.204.97.54,52.5.190.19,54.197.234.188,54.208.100.253,54.208.102.37,107.21.1.8 ~ Fake DuckDuckGo Bot!",
+    "duckduckgo ~ ~ 20.191.45.212,23.21.227.69,40.88.21.235,50.16.241.113,50.16.241.114,50.16.241.117,50.16.247.234,52.204.97.54,52.5.190.19,54.197.234.188,54.208.100.253,54.208.102.37,107.21.1.8 ~ Fake DuckDuckGo Bot!",
     "mail.ru ~ mail.ru ~ ~ Fake Mail Bot!"
   ],
   "user_bot": 0,
